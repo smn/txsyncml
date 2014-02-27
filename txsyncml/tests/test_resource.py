1c843d8fe87013f29244963663c4278056c5d2c4
Simon de Haan <simon@praekeltfoundation.org>
2014-02-27 12:55:11 +0100

    posting syncml w/ proper content-type

--- txsyncml/tests/test_resource.py
+++ txsyncml/tests/test_resource.py
@@ -1,12 +1,13 @@
-import os.path
 import pkg_resources
 
 from twisted.internet import reactor
+from twisted.internet.defer import inlineCallbacks
 from twisted.internet.task import Clock
 from twisted.trial.unittest import TestCase
 from twisted.web.client import HTTPConnectionPool
 from twisted.web import server
 
+from treq import content
 from treq.client import HTTPClient
 
 from txsyncml.resource import TxSyncMLResource
@@ -15,11 +16,10 @@ from txsyncml.resource import TxSyncMLResource
 class TxSyncMLResourceTestCase(TestCase):
 
     def setUp(self):
-        self.clock = Clock()
-        self.pool = HTTPConnectionPool(self.clock, persistent=False)
+        self.pool = HTTPConnectionPool(reactor, persistent=False)
         self.client = HTTPClient.with_config(
-            pool=self.pool, allow_redirects=False, reactor=self.clock)
-        self.resource = TxSyncMLResource(self.clock)
+            pool=self.pool, allow_redirects=False, reactor=reactor)
+        self.resource = TxSyncMLResource(reactor)
         self.site = server.Site(self.resource)
         self.listener = reactor.listenTCP(0, self.site, interface='localhost')
         self.listener_port = self.listener.getHost().port
@@ -38,12 +38,19 @@ class TxSyncMLResourceTestCase(TestCase):
             self.listener_port, ('/'.join(map(str, paths)) + '/'
                                  if paths else ''))
 
-    def send_syncml(self, fixture_name):
-        self.client.post(
-            self.make_url(), data=self.get_fixture(fixture_name),
-            headers={
-                'Content-Type': ['application/vnd.syncml+xml'],
-            })
+    def send_syncml(self, fixture_name, headers=None):
+        default_headers = {
+            'Content-Type': ['application/vnd.syncml+xml'],
+        }
+        if headers is not None:
+            default_headers.update(headers)
 
+        return self.client.post(
+            self.make_url(), data=self.get_fixture(fixture_name),
+            headers=default_headers)
+
+    @inlineCallbacks
     def test_client_sync_init(self):
-        print self.make_url('foo')
+        response = yield self.send_syncml('client_sync_init.xml')
+        print dict(response.headers.getAllRawHeaders())
+        print (yield content(response))
